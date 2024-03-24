5ae2523348952c9a200ecc0bc2f82e053a1b745c
unknown <awwal4md@gmail.com>
2024-03-24 17:27:50 +0100

    slider added

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function toggleMenu() {
   var menu = document.getElementById("menu");
   menu.classList.toggle("active");
 }
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var slides = document.getElementsByClassName("slide");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].classList.add("active");
+  setTimeout(showSlides, 5000); // Change image every 3 seconds
+}
